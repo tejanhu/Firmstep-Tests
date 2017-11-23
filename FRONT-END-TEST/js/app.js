b759152d643c316d0bc4287a2286644961df09ca
Hussein <tejanahmedhu@gmail.com>
2017-11-23 10:34:47 +0000

    commit code

---
+++ FRONT-END-TEST/js/app.js
@@ -0,0 +1,19 @@
+$(function	(){
+	//array filled with objects with product info 
+	 var products = [],
+
+	//the filters object will hold an array of values for each filter
+
+		// E.G:
+		// filters = {
+		// 		"manufacturer" = ["Apple","Sony"],
+		//		"storage" = [16]
+		//	}
+
+		filters = {};
+
+	//event handlers to help with frontend navigation
+
+	var checkboxes = $('.entire-products input[type=checkbox]');
+
+});
